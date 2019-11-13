37fd879b1872f2c07e19c2db2f211e5f2b0fd579
NekoChips <48819393+NekoChips@users.noreply.github.com>
2019-11-13 09:57:59 +0800

    [submitter] NekoChips [submissions] SpringBoot-Redis简单整合

---
+++ springboot-redis/src/main/java/com/demo/redis/RedisApplication.java
@@ -0,0 +1,22 @@
+package com.demo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: RedisApplication <br/>
+ * Description: RedisApplication 相关<br/>
+ * date: 2019/11/12 18:00<br/>
+ *
+ * @author Chenyangjie<br />
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class RedisApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RedisApplication.class);
+    }
+}
---
+++ springboot-redis/src/test/java/com/demo/redis/core/TestRedisTool.java
@@ -0,0 +1,100 @@
+package com.demo.redis.core;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.redis.RedisApplication;
+
+/**
+ * ClassName: TestRedisTool <br/>
+ * Description: TestRedisTool 相关<br/>
+ * date: 2019/11/12 22:18<br/>
+ *
+ * @author Chenyangjie<br />
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RedisApplication.class)
+public class TestRedisTool
+{
+    @Autowired
+    private RedisTool redisTool;
+
+    @Test
+    public void testString()
+    {
+        redisTool.putString("key1", "strValue1", 60L);
+        // Thread.sleep(1000);
+        System.out.println("string : " + redisTool.getValue("key1"));
+    }
+
+    @Test
+    public void testList()
+    {
+        List<String> languages = Arrays.asList("Java", "Python", ".Net", "C#", "C++");
+        redisTool.putList("key2", languages, null);
+        System.out.println("list : " + redisTool.getList("key2"));
+    }
+
+    @Test
+    public void testHash()
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            redisTool.putHash("key3", "hashKey" + i, "hashValue" + i);
+        }
+        System.out.println("allHashValue : " + redisTool.getAllHashValue("key3"));
+        System.out.println("oneHash : " + "hashKey3 -- " + redisTool.getValue("key3", "hashKey3"));
+    }
+
+    @Test
+    public void testSet()
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            redisTool.putSet("key4", "setValue" + i);
+        }
+        System.out.println("set : " + redisTool.getSet("key4"));
+    }
+
+    @Test
+    public void testZset()
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            redisTool.putZSet("key5", "zsetValue" + i, Math.random());
+        }
+        System.out.println("zset : " + redisTool.getZSet("key5", 0d, 0.5d));
+    }
+
+    @Test
+    public void testDelete()
+    {
+        redisTool.delete("key2");
+        System.out.println("list : " + redisTool.getList("key2"));
+
+        redisTool.delete("key3", "hashKey3");
+        System.out.println("allHashValue : " + redisTool.getAllHashValue("key3"));
+        System.out.println("oneHash : " + "hashKey3 -- " + redisTool.getValue("key3", "hashKey3"));
+    }
+
+    @Test
+    public void testSetExpireTime() throws InterruptedException
+    {
+        redisTool.setExpireTime("key3", 10L);
+        redisTool.setExpireTime("key4", 10L);
+        redisTool.setExpireTime("key5", 10L);
+
+        Thread.sleep(10000);
+        System.out.println("allHashValue : " + redisTool.getAllHashValue("key3"));
+        System.out.println("set : " + redisTool.getSet("key4"));
+        System.out.println("zset : " + redisTool.getZSet("key5", 0d, 1d));
+    }
+}
