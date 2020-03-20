ce61b256df8807665af3ce77b9323d00130623a7
NekoChips <shupian421182@outlook.com>
2020-03-20 17:38:25 +0800

    springcloud zuul demo with nacos register

---
+++ springcloud-zuul/src/main/java/com/demo/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.demo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author NekoChips
+ * @description Zuul网关启动类
+ * @date 2020/3/20
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
