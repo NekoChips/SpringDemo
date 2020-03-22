86a9e5603286dcea9ef7b85d8b8ea6c9627e631a
NekoChips <shupian421182@outlook.com>
2020-03-22 16:36:59 +0800

    微调认证服务器配置

--- springcloud-zuul-oauth2/auth-server/src/main/java/com/neko/auth/config/AuthorizationServerConfig.java
+++ springcloud-zuul-oauth2/auth-server/src/main/java/com/neko/auth/config/AuthorizationServerConfig.java
@@ -80,7 +80,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security
-                .tokenKeyAccess("permitAll()")
+                .tokenKeyAccess("isAuthenticated()")
                 .checkTokenAccess("isAuthenticated()");
     }
 }
