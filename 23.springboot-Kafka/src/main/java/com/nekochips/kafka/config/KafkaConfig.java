6dfa5beda8306eb9846cb2a59043a8be6b6df4ae
NekoChips <shupian421182@outlook.com>
2020-04-30 21:41:42 +0800

    Spring Boot 整合 kafka

--- 23.springboot-Kafka/src/main/java/com/nekochips/kafka/config/KafkaConfig.java
+++ 23.springboot-Kafka/src/main/java/com/nekochips/kafka/config/KafkaConfig.java
@@ -18,15 +18,40 @@ import java.util.Map;
  */
 @Configuration
 @EnableKafka
-public class KafkaConfig{
+public class KafkaConfig {
 
     @Autowired
     private KafkaProperties kafkaProperties;
-    
+
     @Bean
     public AdminClient adminClient() {
         Map<String, Object> configs = new HashMap<>(2);
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         return AdminClient.create(configs);
     }
+
+//    @Bean
+//    public ProducerFactory<String, Order> producerFactory() {
+//        Map<String, Object> configs = new HashMap<>(8);
+//        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+//        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        return new DefaultKafkaProducerFactory<>(configs);
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String, Order> consumerFactory() {
+//        Map<String, Object> configs = new HashMap<>(4);
+//        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+//        configs.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
+//        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
+//        return new DefaultKafkaConsumerFactory<>(configs, new StringDeserializer(), new JsonDeserializer<>(Order.class));
+//    }
+//    
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, Order> factory() {
+//        ConcurrentKafkaListenerContainerFactory<String, Order> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
 }
--- 23.springboot-Kafka/src/main/java/com/nekochips/kafka/listener/KafkaMessageListener.java
+++ 23.springboot-Kafka/src/main/java/com/nekochips/kafka/listener/KafkaMessageListener.java
@@ -1,6 +1,10 @@
 package com.nekochips.kafka.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nekochips.kafka.bean.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -16,10 +20,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class KafkaMessageListener {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     /// 监听单个 topic，指定一个 consumer group
 //    @KafkaListener(topics = "kafka_test", groupId = "kafka-consumer")
 //    public void listen(String message) {
-//        log.info("收到消息： {}", message)  
+//        log.info("收到消息： {}", message);  
 //    }
 
     /// 监听多个 topic，指定一个 consumer group，使用 @Header 获取消息来源信息
@@ -27,14 +34,26 @@ public class KafkaMessageListener {
 //    public void listen(@Payload String message,
 //                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
 //                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-//        log.info("收到消息来自 topic: {}, partition: {} 的消息： {}", topic, partition, message);
+//        log.info("收到来自 topic: {}, partition: {} 的消息： {}", topic, partition, message);
 //    }
 
+    /// 使用 JsonDeserializer 反序列化
+//    @KafkaListener(topics = {"kafka_test", "kafka_test2"}, groupId = "kafka-consumer")
+//    public void listen(@Payload Order order,
+//                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+//                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+//        log.info("收到来自 topic: {}, partition: {} 的订单消息： {}", topic, partition, order);
+//    }
 
     @KafkaListener(topics = {"kafka_test", "kafka_test2"}, groupId = "kafka-consumer")
     public void listen(@Payload String message,
                        @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        log.info("收到消息来自 topic: {}, partition: {} 的消息： {}", topic, partition, message);
+        try {
+            Order order = objectMapper.readValue(message, Order.class);
+            log.info("收到来自 topic: {}, partition: {} 的订单消息： {}", topic, partition, order);
+        } catch (JsonProcessingException e) {
+            log.error("parse json error, json string: {}", message, e);
+        }
     }
 }
